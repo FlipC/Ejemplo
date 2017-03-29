04fa8f9fecc4936d3762c22df585e5f9fc1a35f4
Felipe Cifuentes Espinosa <felcifue@alumnos.ubiobio.cl>
2017-03-29 12:25:11 -0300

    Se agrega test con numero negativo

--- src/test/java/cl/ubb/testing/testMyMath.java
+++ src/test/java/cl/ubb/testing/testMyMath.java
@@ -18,4 +18,17 @@ public class testMyMath {
 		assertEquals(resultado, 4);
 		
 	}
+	
+	@Test
+	public void AddMinusOneAndTwoIsThree(){  //prueba de la suma de -1y4
+		//arrage
+		MyMath myMath = new MyMath();
+		
+		//act
+		int resultado= myMath.add(-1, 4);
+		
+		//assert
+		assertEquals(resultado, 3);
+	
+	}
 }
